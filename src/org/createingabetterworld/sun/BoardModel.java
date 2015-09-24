945a17876775580db4abdb01fe261f298e2e884b
Sun <sun@creatingabetterworld.org>
2015-09-24 13:04:59 +0200

    hardcoded puzzle

--- src/org/createingabetterworld/sun/BoardModel.java
+++ src/org/createingabetterworld/sun/BoardModel.java
@@ -32,22 +32,38 @@ public class BoardModel extends AbstractTableModel{
 			}
 			
 		}*/
-		intBoard[0][0] = 0;
-		intBoard[0][1] = 0;
-		intBoard[0][2] = 3;
-		intBoard[0][3] = 4;
-		intBoard[1][0] = 3;
+		intBoard[0][1] = 2;
+		intBoard[0][3] = 1;
+		intBoard[0][4] = 7;
+		intBoard[0][5] = 8;
+		intBoard[0][7] = 3;
 		intBoard[1][1] = 4;
-		intBoard[1][2] = 0;
-		intBoard[1][3] = 0;
-		intBoard[2][0] = 0;
-		intBoard[2][1] = 0;
-		intBoard[2][2] = 4;
-		intBoard[2][3] = 3;
-		intBoard[3][0] = 3;
-		intBoard[3][3] = 4;
-		intBoard[3][2] = 0;
-		intBoard[3][0] = 0;
+		intBoard[1][3] = 3;
+		intBoard[1][5] = 2;
+		intBoard[1][7] = 9;
+		intBoard[2][0] = 1;
+		intBoard[2][8] = 6;
+		intBoard[3][2] = 8;
+		intBoard[3][3] = 6;
+		intBoard[3][5] = 3;
+		intBoard[3][6] = 5;
+		intBoard[4][0] = 3;
+		intBoard[4][8] = 4;
+		intBoard[5][2] = 6;
+		intBoard[5][3] = 7;
+		intBoard[5][5] = 9;
+		intBoard[5][6] = 2;
+		intBoard[6][0] = 9;
+		intBoard[6][8] = 2;
+		intBoard[7][1] = 8;
+		intBoard[7][3] = 9;
+		intBoard[7][5] = 1;
+		intBoard[7][7] = 6;
+		intBoard[8][1] = 1;
+		intBoard[8][3] = 4;
+		intBoard[8][4] = 3;
+		intBoard[8][5] = 6;
+		intBoard[8][7] = 5;
 	}
 	/**
 	 * @param args
